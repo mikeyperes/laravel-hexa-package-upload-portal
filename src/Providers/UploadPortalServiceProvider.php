1b0f26bf28ead5795b7607c86b84e6cb1dc53bc9
mikeyperes <michael@mike-ro-tech.com>
2026-04-11 00:47:19 -0400

    Add settingsShellClass to package metadata

--- src/Providers/UploadPortalServiceProvider.php
+++ src/Providers/UploadPortalServiceProvider.php
@@ -41,6 +41,7 @@ class UploadPortalServiceProvider extends ServiceProvider
                 'icon' => 'M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12',
                 'description' => 'Reusable upload component package for multi-file uploads, temp storage, and galleries.',
                 'settingsRoute' => 'upload-portal.settings',
+            'settingsShellClass' => 'max-w-3xl',
                 'docsSlug' => 'upload-portal',
                 'instructions' => [
                     'Embed the upload component where file collection is needed.',
